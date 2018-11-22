2efc26652a4f69bf3e2b621196d5e07f94a93bef
vesper0990 <kamilsoloducha@gmail.com>
2018-11-22 17:56:43 +0100

    Add commands to script about removing docker images

--- build/scripts/remove_containers_and_images.sh
+++ build/scripts/remove_containers_and_images.sh
@@ -1,3 +1,5 @@
 docker stop $(docker container ls -a -q --filter ancestor="wordki")
 docker rm $(docker container ls -a -q --filter ancestor="wordki")
 docker rm $(docker container ls -a -q --filter ancestor="wordki.builder")
+docker image rm wordki
+docker image rm wordki.builder
