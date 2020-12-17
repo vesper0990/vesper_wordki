2cd7a13a9ba66ee6917f13d430848e167f1d38ea
Kamil <kamilsoloducha@gmail.com>
2020-12-17 17:15:18 +0100

    add admin controller

--- server/src/Wordki.Api/Repositories/EntityFrameworkRepositories/WordkiDbContext.cs
+++ server/src/Wordki.Api/Repositories/EntityFrameworkRepositories/WordkiDbContext.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using Wordki.Api.Domain;
 
 namespace Wordki.Api.Repositories.EntityFrameworkRepositories
