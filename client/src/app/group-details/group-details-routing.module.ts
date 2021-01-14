076779206f8166ac1aa0dfa78361693084d2264f
Kamil <kamilsoloducha@gmail.com>
2021-01-14 16:21:08 +0100

    fix errors

--- client/src/app/group-details/group-details-routing.module.ts
+++ client/src/app/group-details/group-details-routing.module.ts
@@ -1,13 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { GroupDetailsComponent } from './group-details.component';
-import { AddGroupComponent } from './pages/add-group/add-group.component';
 
 const routes: Routes = [
-  {
-    path: 'add',
-    component: AddGroupComponent
-  },
   {
     path: ':id',
     component: GroupDetailsComponent
